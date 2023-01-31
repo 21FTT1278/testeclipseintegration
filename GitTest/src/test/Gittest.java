059a22ce74ffd49b7d4e4dfb4b4268df170ee666
Wan Wafiy Hasif <21FTT1278@student.pb.edu.bn>
2023-01-31 15:28:44 +0800

    I finally understand OOP WOWWWW!

--- GitTest/src/test/Gittest.java
+++ GitTest/src/test/Gittest.java
@@ -4,5 +4,7 @@ package test;
 public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("Meow meow meow aughhuaghahhhhh");
+		ClassA classa = new ClassA("ABCDEFGHIJKLMNOP");
+		System.out.println(classa.getAbc());
 	}
 }
