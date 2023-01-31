7a2622b0e31b112d4feb4570b8619e4553cae1e5
Wan Wafiy Hasif <21FTT1278@student.pb.edu.bn>
2023-01-31 16:10:39 +0800

    hit the griddy

--- GitTest/src/test/Gittest.java
+++ GitTest/src/test/Gittest.java
@@ -4,7 +4,7 @@ package test;
 public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("Meow meow meow aughhuaghahhhhh");
-		ClassA classa = new ClassA("ABCDEFGHIJKLMNOP");
+		ClassA classa = new ClassA("I SAY RIGHT FOOT I SAY RIGHT FOOT");
 		System.out.println(classa.getAbc());
 	}
 }
