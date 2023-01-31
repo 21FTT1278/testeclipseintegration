14068f90bb223383c76cb597c5ae1eb36ca61841
Amirul Aiman <21FTT1273@student.pb.edu.bn>
2023-01-31 16:07:59 +0800

    dergs

--- GitTest/src/test/Gittest.java
+++ GitTest/src/test/Gittest.java
@@ -4,7 +4,7 @@ package test;
 public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("Meow meow meow aughhuaghahhhhh");
-		ClassA classa = new ClassA("ABbababababababa");
+		ClassA classa = new ClassA("uwu rawr x3 i like dergs galore nyehehehe");
 		System.out.println(classa.getAbc());
 	}
 }
