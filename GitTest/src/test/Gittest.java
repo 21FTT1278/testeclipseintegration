ffa327f83e21709607578d2d0312082e25ac3520
Amirul Aiman <21FTT1273@student.pb.edu.bn>
2023-01-31 15:52:45 +0800

    yuh boi yuh

--- GitTest/src/test/Gittest.java
+++ GitTest/src/test/Gittest.java
@@ -4,7 +4,7 @@ package test;
 public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("Meow meow meow aughhuaghahhhhh");
-		ClassA classa = new ClassA("ABCDEFGHIJKLMNOP");
+		ClassA classa = new ClassA("ABbababababababa");
 		System.out.println(classa.getAbc());
 	}
 }
